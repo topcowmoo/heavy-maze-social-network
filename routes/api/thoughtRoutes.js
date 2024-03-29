7627b15d72437b0733c3f4f963d11ff00ce4aea8
Salvatore <mammoliti.sam@gmail.com>
2024-03-29 18:19:43 -0400

    Add routes for managing thoughts and reactions

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+const {
+  getAllThought,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thoughtController');
+
+router
+  .route('/')
+  .get(getAllThought)
+  .post(createThought);
+
+router
+  .route('/:id')
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
