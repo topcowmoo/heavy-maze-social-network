724fea3907620620ca7a1d6317b4ed7710dfc500
Salvatore <mammoliti.sam@gmail.com>
2024-03-29 15:31:52 -0400

    Completed index.js file to set up connection with express

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+const db = require('./config/connection');
+const routes = require('./routes');
 
+const PORT = process.env.PORT || 3001;
+const express = require('express');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
 
-
-
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
 
 // npm run dev
+
+// env file 
